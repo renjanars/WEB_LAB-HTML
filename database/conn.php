966d93c3eca4ee4fa5fd6b864ed34449730cf4db
Renjana <119409589+renjanars@users.noreply.github.com>
2023-01-19 12:18:19 +0530

    Add files via upload

---
+++ database/conn.php
@@ -0,0 +1,17 @@
+
+
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="renjana";
+$conn=mysqli_connect($servername,$username,$password,$dbname);
+if(!$conn)
+{
+die("Connection failed!".mysqli_connect_error());
+}
+else
+{
+ echo "Database connected";
+}
+?>
